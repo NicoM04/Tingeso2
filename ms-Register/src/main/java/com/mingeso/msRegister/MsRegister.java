7cb4057295536a679870f70876e84c60a6dfa5e5
Nicokraken <nicolas.morales.r@usach.cl>
2024-12-08 22:09:04 -0300

    commit 4

--- ms-Register/src/main/java/com/mingeso/msRegister/MsRegister.java
+++ ms-Register/src/main/java/com/mingeso/msRegister/MsRegister.java
@@ -2,8 +2,10 @@ package com.mingeso.msRegister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsRegister {
 
 	public static void main(String[] args) {
